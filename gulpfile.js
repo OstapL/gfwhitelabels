2909fe66ed158679c9666dc42d6f66edcf0136ed
Vladyslav <vladka@webdevelop.pro>
2016-08-01 12:39:21 -0400

    Обновил файл packages.json - всем сдселать npm install

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,19 @@
-var gulp         = require('gulp'),
-    sass         = require('gulp-sass'),
-    autoprefixer = require('gulp-autoprefixer'),
+var gulp        = require('gulp'),
+    sass        = require('gulp-sass'),
+    autoprefixer= require('gulp-autoprefixer'),
     cleanCSS    = require('gulp-clean-css'),
-    rename       = require('gulp-rename'),
-    browserSync  = require('browser-sync').create(),
-    concat       = require('gulp-concat'),
-    uglify       = require('gulp-uglify');
+    rename      = require('gulp-rename'),
+    browserSync = require('browser-sync').create(),
+    concat      = require('gulp-concat'),
+    uglify      = require('gulp-uglify');
+    pug         = require('gulp-pug');
+    path        = require('path');
 
 
 var config = {
     bootstrapDir: './node_modules/bootstrap/scss',
+    tatherDir: './node_modules/tether',
+    fontAwesomeDir: './node_modules/font-awesome/scss',
     publicDir: './app/html',
 };
 
@@ -25,7 +29,11 @@ gulp.task('browser-sync', ['styles', 'scripts'], function() {
 gulp.task('styles', function () {
     return gulp.src('app/sass/mixins_all.sass')
     .pipe(sass({
-        includePaths: [config.bootstrapDir, './node_modules/tether/',  './node_modules/font-awesome/scss']
+        includePaths: [
+            config.bootstrapDir, 
+            config.tetherDir,  
+            config.fontAwesomeDir
+        ]
     }).on('error', sass.logError))
     .pipe(rename({basename: 'main', prefix : ''}))
 // .pipe(autoprefixer({browsers: ['last 15 versions'], cascade: false}))
@@ -36,17 +44,49 @@ gulp.task('styles', function () {
 
 gulp.task('scripts', function() {
     return gulp.src([
-    ])
-    .pipe(concat('libs.js'))
-    // .pipe(uglify()) //Minify libs.js
-    .pipe(gulp.dest(config.publicDir + '/js/'));
+        'app/bethesda/config.js',
+        'node_modules/jquery/dist/jquery.js',
+        'node_modules/underscore/underscore.js',
+        'node_modules/backbone/backbone.js',
+        'node_modules/backbone-validation/dist/backbone-validation.js',
+        'app/bethesda/utils.js',
+        'node_modules/tether/dist/js/tether.js',
+        'node_modules/bootstrap/dist/js/bootstrap.js',
+        'node_modules/requirejs/require.js',
+        'app/bethesda/models/user.js',
+        'app/bethesda/controllers/app.js'
+        ])
+        .pipe(concat('libs.js'))
+        //.pipe(uglify()) //Minify libs.js
+        .pipe(gulp.dest(config.publicDir + '/js'));
 });
+
+gulp.task('views', function buildHTML() {
+  return gulp.src('./app/bethesda/templates/*.jade')
+    .pipe(rename(function (path) {  
+        path.extname = ".js"
+    }))
+    .pipe(pug({
+          client: 1,
+          // To Do Ask a question: name: path.basename(file.path).replace('.js', ''),
+          name: 'newFunc',
+          /*
+          globals: {
+            serverUrl: "http://192.168.0.100:8000",
+          },
+          */
+          debug: 0,
+          compileDebug: 0,
+    }))
+    .pipe(gulp.dest(config.publicDir + '/compiled_t'));
+});
+
 
 gulp.task('watch', function () {
     gulp.watch(['app/sass/*.sass', 'app/sass/**/*.sass',], ['styles']);
-    // gulp.watch('app/libs/**/*.js', ['scripts']);
-    // gulp.watch('app/js/*.js').on("change", browserSync.reload);
+    gulp.watch(['app/bethesda/*.js', 'app/bethesda/**/*.js'], ['scripts'])
+    gulp.watch(['app/bethesda/templates/*.jade', ], ['views'])
     // gulp.watch('app/*.html').on('change', browserSync.reload);
 });
 
-gulp.task('default', ['browser-sync', 'watch']);
+gulp.task('default', ['watch']);
