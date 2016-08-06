663a7bc57734b1d01fa4715fcde4ee9b8ea1c6a4
OstapL <ostap-v-30@mail.ru>
2016-08-06 23:22:59 +0300

    табы scroll - нужно переделать, страница campaign_detail

--- app/html/js/common.js
+++ app/html/js/common.js
@@ -12,4 +12,39 @@ $('#toggle_click').click(function()
         }
     });
 
-}); 
+
+
+
+});
+$(document).ready(function(){
+
+        var $menu = $(".tabs-scroll");
+
+        $(window).scroll(function(){
+            if ( $(this).scrollTop() > 100 && $menu.hasClass("navbar-default") ){
+                $menu.fadeOut('fast',function(){
+                    $(this).removeClass("navbar-default")
+                           .addClass("menu-fixed transbg")
+                           .fadeIn('fast');
+                });
+            } else if($(this).scrollTop() <= 100 && $menu.hasClass("menu-fixed")) {
+                $menu.fadeOut('fast',function(){
+                    $(this).removeClass("menu-fixed transbg")
+                           .addClass("navbar-default")
+                           .fadeIn('fast');
+                });
+            }
+        });//scroll
+
+        $menu.hover(
+            function(){
+                if( $(this).hasClass('menu-fixed') ){
+                    $(this).removeClass('transbg');
+                }
+            },
+            function(){
+                if( $(this).hasClass('menu-fixed') ){
+                    $(this).addClass('transbg');
+                }
+            });//hover
+    });//jQuery
