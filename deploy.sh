85f12135edb5dd5ec67ad82f557613560c1eb715
Vladyslav <vladka@webdevelop.pro>
2016-08-13 20:13:36 -0400

    fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 DEFAULT="default"
 PROFILE=${AWS_PROFILE:-$DEFAULT}
 BUCKET=growthfountain-frontend
-DIR=/Users/admin/projects/gfwhitelabels/app/html
-aws  s3 cp $DIR s3://$BUCKET/ --profile "$PROFILE"  --recursive
-
+ls
+DIR=/
+#aws  s3 cp $DIR s3://$BUCKET/ --profile "$PROFILE"  --recursive
