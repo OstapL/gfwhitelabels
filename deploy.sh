623ba191589b76fcd3cf7f4eb7de0ad437a34055
Vladyslav <vladka@webdevelop.pro>
2016-08-13 20:19:05 -0400

    update circle config and moved html to dist

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@ DEFAULT="default"
 PROFILE=${AWS_PROFILE:-$DEFAULT}
 BUCKET=growthfountain-frontend
 ls
-DIR=/
-#aws  s3 cp $DIR s3://$BUCKET/ --profile "$PROFILE"  --recursive
+DIR=/app/dist
+aws  s3 cp $DIR s3://$BUCKET/ --profile "$PROFILE"  --recursive
--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 var express = require("express");
-var app     = express();
 var path    = require("path");
+var app     = express();
 
 
-app.use("/img", express.static('app/html/img'));
+app.use("/img", express.static('app/dist/img'));
 app.use("/node_modules", express.static('node_modules'));
-app.use("/css", express.static('app/html/css'));
-app.use("/js", express.static('app/html/js'));
+app.use("/css", express.static('app/dist/css'));
+app.use("/js", express.static('app/dist/js'));
 app.use("/views", express.static('app/bethesda/views'));
 app.use("/models", express.static('app/bethesda/models'));
 app.use("/templates_js", express.static('app/bethesda/templates'));
-app.use("/templates", express.static('app/html/templates'));
+app.use("/templates", express.static('app/dist/templates'));
 
 app.get('/*',function(req,res){
   res.sendFile(path.join(__dirname+'/app/server.html'));
