4cdafcd173db00ba1c77071987c9e93d021f1c73
Vladyslav <vladka@webdevelop.pro>
2016-08-16 16:11:01 -0400

    updated cicrcle

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 DEFAULT="default"
 PROFILE=${AWS_PROFILE:-$DEFAULT}
-BUCKET=growthfountain-frontend
-ls
+BUCKET="growthfountain-$CIRCLE_BRANCH"
 DIR=dist
-aws  s3 cp $DIR s3://$BUCKET/ --profile "$PROFILE"  --recursive
+aws  s3 sync $DIR s3://$BUCKET/ --profile "$PROFILE"  --recursive
 DIR=app/dist
-aws  s3 cp $DIR s3://$BUCKET/ --profile "$PROFILE"  --recursive
+aws  s3 sync $DIR s3://$BUCKET/ --profile "$PROFILE"  --recursive
