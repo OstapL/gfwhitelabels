a0b6f9541690f5a7b6b5ede461f159f002bcd0a4
Vladyslav <vladka@webdevelop.pro>
2016-08-23 12:42:45 -0400

    fix image deploy

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ PROFILE=${AWS_PROFILE:-$DEFAULT}
 BUCKET="growthfountain-$CIRCLE_BRANCH"
 DIR=dist
 aws  s3 sync $DIR s3://$BUCKET/ --profile "$PROFILE"
-DIR=app/dist
-aws  s3 sync $DIR s3://$BUCKET/ --profile "$PROFILE"
+DIR=src/img/
+aws  s3 sync $DIR s3://$BUCKET/img/ --profile "$PROFILE"
 echo  'done'
