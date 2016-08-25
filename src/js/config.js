2cb6307c7735d3bc8f7d92a55f0ca75d3ee7ae46
Vladyslav <vladka@webdevelop.pro>
2016-08-25 17:29:56 -0400

    update linkedin credentials

--- src/js/config.js
+++ src/js/config.js
@@ -2,4 +2,4 @@ global.serverUrl = "http://jd-django.jwpbirm43d.us-east-1.elasticbeanstalk.com";
 //global.serverUrl = "http://192.168.99.100:8000";
 global.facebookClientId = "1405768896335643";
 global.googleClientId = "329026937492-pkmge3at186jvjn14mep9vpa4fk3gqpg.apps.googleusercontent.com";
-global.linkedinClientId = "77hgtbs9rkjk20";
+global.linkedinClientId = "77klh0puabx98s";
