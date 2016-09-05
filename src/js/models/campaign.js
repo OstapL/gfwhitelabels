6fd2633a4c44373237faedca0e75e6ae71f415a8
Arthur Yip <arthuryip723@gmail.com>
2016-09-05 16:36:40 -0400

    fix another typo

--- src/js/models/campaign.js
+++ src/js/models/campaign.js
@@ -29,13 +29,13 @@ define(function() {
                 return formatPrice(number);
             },
 
-            month: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+            months: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
 
             formatDate: function (dateStr) {
                 var strs = dateStr.split("-");
                 var monthIndex = parseInt(strs[1]) - 1;
                 // return strs[1] + "-" + strs[0];
-                return this.month[monthIndex] + " " + strs[0];
+                return this.months[monthIndex] + " " + strs[0];
             },
 
             calculateRaisedPercentage: function (minimum_raise, amount_raised) {
