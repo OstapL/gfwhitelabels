abb1dfadceea5bc25202aec299603aaf67c4bb1c
Denis Chernienko <denekurich@gmail.com>
2016-09-15 20:17:37 +0300

    done

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ global.serverUrl = "http://jd-django.jwpbirm43d.us-east-1.elasticbeanstalk.com";
 //global.serverUrl = "http://gfauth.com:8000";
 
 
-bal.facebookClientId = "191471871275050";
+global.facebookClientId = "191471871275050";
 global.googleClientId = "488593151885-87nqfd8gl444a1me0n149otrf37dbahq.apps.googleusercontent.com";
 global.linkedinClientId = "77hgtbs9rkjk20"
 global.googleMapKey = "AIzaSyBpCl9-7bkVISZ0o-AaFCsKzZwGAxalkZU"
