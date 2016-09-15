bb332ec76d5e065b67db61dabcbf75ed3862e88f
Denis Chernienko <denekurich@gmail.com>
2016-09-15 22:11:41 +0300

    updated linked in client id

--- src/config.js
+++ src/config.js
@@ -5,5 +5,5 @@ global.serverUrl = "http://jd-django.jwpbirm43d.us-east-1.elasticbeanstalk.com";
 
 global.facebookClientId = "191471871275050";
 global.googleClientId = "488593151885-87nqfd8gl444a1me0n149otrf37dbahq.apps.googleusercontent.com";
-global.linkedinClientId = "77hgtbs9rkjk20"
+global.linkedinClientId = "77wzj6tz0yyr33";
 global.googleMapKey = "AIzaSyBpCl9-7bkVISZ0o-AaFCsKzZwGAxalkZU"
