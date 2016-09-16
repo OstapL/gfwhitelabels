c5d1288e2e3fce98f97998051998d9286f0baf13
Vladyslav <vladka@webdevelop.pro>
2016-09-16 12:02:07 -0400

    updated deploy for beta growth

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,10 @@ DIR=dist
 aws  s3 sync $DIR s3://$BUCKET/ --profile "$PROFILE"
 DIR=src/img/
 aws  s3 sync $DIR s3://$BUCKET/img/ --profile "$PROFILE"
+
+BUCKET="beta.growthfountain.com"
+DIR=dist
+aws  s3 sync $DIR s3://$BUCKET/ --profile "$PROFILE"
+DIR=src/img/
+aws  s3 sync $DIR s3://$BUCKET/img/ --profile "$PROFILE"
 echo  'done'
