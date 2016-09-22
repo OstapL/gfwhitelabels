c0c53181897cd2d13214d5e5f689f216f5712deb
Vladyslav <vladka@webdevelop.pro>
2016-09-22 15:40:54 -0400

    Fixed profile area

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 global.serverUrl = "https://django-api.growthfountain.com";
 global.teamName = 'Team';
-//global.serverUrl = "http://192.168.99.100:8000";
+global.serverUrl = "http://192.168.99.100:8000";
 //global.serverUrl = "http://gfauth.com:8000";
 
 global.facebookClientId = "191471871275050";
