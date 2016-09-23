00647a8fbf0dc111d866ea66d64dbbaae0842aca
Arthur Yip <arthuryip723@gmail.com>
2016-09-23 11:18:16 -0400

    fixed regex in videoUpdate

--- src/components/raiseFunds/views.js
+++ src/components/raiseFunds/views.js
@@ -404,7 +404,8 @@ module.exports = {
           var id;
 
           if (provider == 'youtube') {
-            id = url.match(/https:\/\/(?:www.)?(\w*).com\/.*v=(\w*)/)[2];
+            // id = url.match(/https:\/\/(?:www.)?(\w*).com\/.*v=(\w*)/)[2];
+            id = url.match(/https:\/\/(?:www.)?(\w*).com\/.*v=([A-Za-z0-9_-]*)/)[2];
           } else if (provider == 'vimeo') {
             id = url.match(/https:\/\/(?:www.)?(\w*).com\/(\d*)/)[2];
           } else {
