5c251893a815e438377d94393246aeeb14661dff
Arthur Yip <arthuryip723@gmail.com>
2016-09-29 10:30:11 -0400

    tweak the menu a bit

--- src/helpers/menuHelper.js
+++ src/helpers/menuHelper.js
@@ -13,9 +13,11 @@ module.exports = {
         let $a = $elem.find('a.list-group-heading');
         let $icon = $a.find('.fa.arrow');
         if (e.type === 'show') {
+          // if (!$elem.hasClass('sub-menu')) $a.addClass('active');
           $a.addClass('active');
           $icon.removeClass('fa-angle-left').addClass('fa-angle-down');
         } else if (e.type === 'hide') {
+          // if (!$elem.hasClass('sub-menu')) $a.removeClass('active');
           $a.removeClass('active');
           $icon.removeClass('fa-angle-down').addClass('fa-angle-left');
         }
