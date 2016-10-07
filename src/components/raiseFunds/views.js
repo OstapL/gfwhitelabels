5bb6a9c33900fc86c2d7ab5727444194e06d15f3
Vladyslav <vladka@webdevelop.pro>
2016-10-07 12:49:14 -0400

    fixed menu for raise funds

--- src/components/raiseFunds/views.js
+++ src/components/raiseFunds/views.js
@@ -285,6 +285,7 @@ module.exports = {
       preinitialize() {
         // ToDo
         // Hack for undelegate previous events
+        debugger;
         for (let k in this.events) {
           $('#content ' + k.split(' ')[1]).undelegate();
         }
@@ -588,14 +589,17 @@ module.exports = {
           };
         }
 
+        this.values.progress = this.model.progress;
+
         this.usaStates = require('helpers/usa-states');
         this.$el.html(
           template({
             serverUrl: serverUrl,
             Urls: Urls,
             fields: this.fields,
+            campaign: this.model,
             member: this.values,
-            values: this.values,
+            values: this.model,
             type: this.type,
             index: this.index,
             states: this.usaStates,
@@ -623,10 +627,11 @@ module.exports = {
         e.preventDefault();
         let json = $(e.target).serializeJSON();
         json.index = this.index;
+        debugger;
 
         api.submitAction.call(this, e, json);
       },
-    })),
+  })),
 
   teamMembers: Backbone.View.extend({
     events: {
