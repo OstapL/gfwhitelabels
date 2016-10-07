9a0f3cd847e5d3d12a26bff1909d8879b5d166b7
Sergey <s@unixowl.com>
2016-10-07 20:51:08 +0300

    sed for s3 bucket name

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@
 DEFAULT="default"
 PROFILE=${AWS_PROFILE:-$DEFAULT}
 BUCKET="growthfountain-$CIRCLE_BRANCH"
+BUCKET=`echo $BUCKET | sed -e "s/_/-/g"`
 DIR=dist
 aws  s3 sync $DIR s3://$BUCKET/ --profile "$PROFILE"
 DIR=src/img/
