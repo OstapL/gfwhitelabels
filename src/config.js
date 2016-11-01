cd6e02211ec6bdc07e8c3b4598c3632bcc9cf69d
Vladyslav <vladka@webdevelop.pro>
2016-11-01 21:07:13 +0200

    try one more time

--- src/config.js
+++ src/config.js
@@ -11,6 +11,7 @@ global.teamTitle = "Meet The Team";
 //global.serverUrl = "http://192.168.99.100:8000";
 //global.serverUrl = "http://gfauth.com:8000";
 
+
 global.facebookClientId = "191471871275050";
 global.googleClientId = "488593151885-87nqfd8gl444a1me0n149otrf37dbahq.apps.googleusercontent.com";
 global.linkedinClientId = "77wzj6tz0yyr33";
