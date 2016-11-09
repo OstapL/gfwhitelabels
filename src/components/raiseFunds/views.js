d5ad3bca29532bee486b74fcacc1ee2e43f18bfb
OstapL <ostap-v-30@mail.ru>
2016-11-09 19:35:32 +0200

    fix bug page specifics

--- src/components/raiseFunds/views.js
+++ src/components/raiseFunds/views.js
@@ -920,6 +920,12 @@ module.exports = {
           minimum_increment: 'The Minimum investment is',
           length_days: 'Length of the Campaign',
           investor_presentation: 'Upload an Investor Presentation',
+          premoney_valuation: 'Pre-Money Valuation',
+          price_per_share: 'Price Per Share',
+          min_number_of_shares: 'Minimum № of Shares',
+          max_number_of_shares: 'Maximum № of Shares',
+          min_equity_offered: 'Minimum Equity Offered',
+          max_equity_offered: 'Maximum Equity Offered',
         };
         this.assignLabels();
         this.createIndexes();
