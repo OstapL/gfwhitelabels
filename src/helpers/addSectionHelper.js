87ca1f65b8bc688d9543850ef6782bd94a1cc377
Vladyslav <vladka@webdevelop.pro>
2016-11-10 15:15:02 +0200

    Fixes from Ken presentation

--- src/helpers/addSectionHelper.js
+++ src/helpers/addSectionHelper.js
@@ -124,7 +124,7 @@ module.exports = {
        */
 
       _(this.fields).each((el, key) => {
-        if(el.type == 'nested' &&  !key.endsWith('_data')) {
+        if(el.type == 'nested' &&  !el.schema.hasOwnProperty('urls')) {
           this.jsonTemplates[key] = require('components/' + component + '/templates/snippets/' + key + '.pug')({
             attr: _.extend(
               {}, this.fields[key]
