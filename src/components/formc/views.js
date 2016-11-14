7b0838a036366ab9a1a0b7d10ab7bd0cdc9a7bf0
Vladyslav <vladka@webdevelop.pro>
2016-11-14 16:19:42 +0200

    check if is_paid or not

--- src/components/formc/views.js
+++ src/components/formc/views.js
@@ -52,12 +52,48 @@ module.exports = {
       'keyup .electronically-sign .user-name': 'changeSign',
       'click #pay-btn': 'stripeSubmit',
     }, menuHelper.events, yesNoHelper.events),
+
     preinitialize() {
       // ToDo
       // Hack for undelegate previous events
       for (let k in this.events) {
         $('#content ' + k.split(' ')[1]).undelegate();
       }
+    },
+
+    initialize(options) {
+      this.fields = options.fields;
+      this.campaign = options.campaign;
+
+      if(this.model.is_paid === false) {
+        this.fields.company_name = {
+          required: true
+        };
+        this.fields.full_name = { 
+          required: true 
+        };
+      }
+    },
+
+    render() {
+      let template = require('./templates/introduction.pug');
+
+      this.$el.html(
+        template({
+          serverUrl: serverUrl,
+          Urls: Urls,
+          fields: this.fields,
+          values: this.model,
+          campaignId: this.campaign.id,
+        })
+      );
+
+      let eSignForm = this.$('.electronically-sign');
+      this.eSignCompanyName = eSignForm.find('#company-name');
+      this.eSignFullName = eSignForm.find('#full-name');
+      this.eSignPreview = eSignForm.find('.electronically .name');
+
+      return this;
     },
 
     getSuccessUrl() {
@@ -175,43 +211,18 @@ module.exports = {
         'full_name': this.eSignFullName.val(),
       };
 
-      // api.makeRequest(formcServer + '/' + this.model.id + '/sign', signData).done(() => {
-        api.submitAction.call(this, e, data);
-      // });
+      if(this.model.is_paid == false) {
+        signData.company_name = this.eSignCompanyName.val();
+        signData.full_name = this.eSignFullName.val();
+      }
+
+      api.submitAction.call(this, e, data);
 
       return false;
     },
 
     changeSign() {
       this.eSignPreview.text(this.eSignFullName.val());
-    },
-
-    initialize(options) {
-      this.fields = options.fields;
-      this.fields.company_name = { required: true };
-      this.fields.full_name = { required: true };
-      this.campaign = options.campaign;
-    },
-
-    render() {
-      let template = require('components/formc/templates/introduction.pug');
-
-      this.$el.html(
-        template({
-          serverUrl: serverUrl,
-          Urls: Urls,
-          fields: this.fields,
-          values: this.model,
-          campaignId: this.campaign.id,
-        })
-      );
-
-      let eSignForm = this.$('.electronically-sign');
-      this.eSignCompanyName = eSignForm.find('#company-name');
-      this.eSignFullName = eSignForm.find('#full-name');
-      this.eSignPreview = eSignForm.find('.electronically .name');
-
-      return this;
     },
 
   }, menuHelper.methods, yesNoHelper.methods)),
