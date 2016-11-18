c54ef6de7ad749b5479d6878b594a50525ca78a0
VladimirC <vladimir.chagin@gmail.com>
2016-11-18 18:44:01 +0200

    remove file forlder dropzone files

--- src/helpers/dropzoneHelpers.js
+++ src/helpers/dropzoneHelpers.js
@@ -48,6 +48,7 @@ module.exports = {
         url: filerUrl + '/upload',
         clickable: '.dropzone__' + name + ' .border-dropzone',
         createImageThumbnails: false,
+        addRemoveLinks: true,
         thumbnail: function (file, dataUrl) {
           console.log('preview', file, file.xhr, file.xhr.response, file.xhr.responseText);
         },
@@ -111,7 +112,6 @@ module.exports = {
           onSuccess(data, file);
         }
       });
-
     },
 
     _notifyServerAboutChanges(name, data) {
@@ -126,6 +126,7 @@ module.exports = {
       //   params
       // );
     },
+
 
     _file(name) {
 
@@ -198,28 +199,85 @@ module.exports = {
         let fieldDataName = name.replace('_id', '_data');
         let url = data[0].urls[0];
 
+        let fileBlock = $('<div class=".thumb-file-container">' +
+          '<div class="delete-file-container" style="position: absolute;">' +
+            '<a href="#" class="delete-file" data-fileid="' + data[0].id + '"><i class="fa fa-times"></i></a>' +
+          '</div>' +
+          '<img class="img-file img-' + name + '" src="/img/icons/' + icon + '.png" />' +
+          '<a class="link-file a-' + name + '" target="_blank" href="' + url + '" title="' + data[0].name +'">' + textHelper.shortenFileName(data[0].name) + '</a>' +
+        '</div>');
+
+        let $link = fileBlock.find('a.delete-file');
+        $link.on('click', (e) => {
+          e.preventDefault();
+          e.stopPropagation();
+
+          let fileId = $link.data('fileid');
+
+          $link.prop('enabled', false);
+          $link.off('click');
+          // api.makeRequest(filerUrl + '/' + fileId, 'DELETE').done(() => {
+              //remove field from model
+              let dataArr = this.model[name.replace('_id', '_data')];
+              let dataIdx = _(dataArr).findIndex((elem) => { return elem.id == fileId});
+              if (dataIdx >= 0) {
+                dataArr.splice(dataIdx, 1);
+                if (!dataArr.length) {
+                  //add empty file
+                  $link.closest('.file-scroll').append('<div class="thumb-file-container col-xl-12">' +
+                    '<img src="/img/icons/nofile.png" alt="" class="img-file img-"' + name + '>' +
+                    '<a class="a-' + name + '" href="#"></a>' +
+                  '</div>');
+                }
+                $link.closest('.thumb-file-container').remove();
+              }
+          // });
+
+          return false;
+        });
+
+        $('.dropzone__' + name + ' .thumb-file-container :last').after(fileBlock);
+
+
+
         if(this.model[fieldDataName].length == 0) {
-          $('.img-' + name).attr('src', '/img/icons/' + icon + '.png');
-          $('.a-' + name).attr('href', url).html(data[0].name);
-        } else {
-          let $removeButton = $('<button type="button" class="close" data-dismiss="modal" aria-label="Close">' +
-            '<span aria-hidden="true">&times;</span>' +
-          '</button>');
-
-          $removeButton.on('click', (e) => {
-            this.removeFile(file);
-            return false;
-          });
-
-          $('.a-' + name + ':last').after(
-            '<div class="col-xl-12">' +
-              '<img class="img-file img-' + name + '" src="/img/icons/' + icon + '.png" />' +
-            '</div>' +
-            '<a class="a-' + name + '" href="' + url + '" title="' + data[0].name +'">' + textHelper.shortenFileName(data[0].name) + '</a>'
-          ).after($removeButton);
+          $('.dropzone__' + name + ' .thumb-file-container :first').remove();
         }
 
         this.model[name.replace('_id', '_data')].push(data[0]);
+      });
+
+      //attach remove event handlers to dropzone items
+      $('.dropzone__' + name + ' .img-dropzone a.delete-file').each((idx, link) => {
+        let $link = $(link);
+
+        $link.on('click', (e) => {
+          e.preventDefault();
+          e.stopPropagation();
+
+          let fileId = $link.data('fileid');
+
+          $link.prop('enabled', false);
+          $link.off('click');
+          // api.makeRequest(filerUrl + '/' + fileId, 'DELETE').done(() => {
+              //remove field from model
+              let dataArr = this.model[name.replace('_id', '_data')];
+              let dataIdx = _(dataArr).findIndex((elem) => { return elem.id == fileId});
+              if (dataIdx >= 0) {
+                dataArr.splice(dataIdx, 1);
+                if (!dataArr.length) {
+                  //add empty file
+                  $link.closest('.file-scroll').append('<div class="thumb-file-container col-xl-12">' +
+                    '<img src="/img/icons/nofile.png" alt="" class="img-file img-"' + name + '>' +
+                    '<a class="a-' + name + '" href="#"></a>' +
+                  '</div>');
+                }
+                $link.closest('.thumb-file-container').remove();
+              }
+          // });
+
+          return false;
+        });
       });
     },
 
