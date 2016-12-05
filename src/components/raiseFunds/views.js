a34212b3d7847b30f21bc4973ad61326586a9fce
OstapL <ostap-v-30@mail.ru>
2016-12-05 14:42:41 +0200

    afterCompleteFillingDashboard

--- src/components/raiseFunds/views.js
+++ src/components/raiseFunds/views.js
@@ -193,6 +193,19 @@ module.exports = {
       return this;
     },
   }),
+  afterCompleteDashboard: Backbone.View.extend({
+    el: '#content',
+    template: require('./templates/afterCompleteFillingDashboard.pug'),
+
+    render() {
+      this.$el.html(
+        this.template({
+          
+        })
+      );
+      return this;
+    },
+  }),
   generalInformation: Backbone.View.extend(_.extend({
       urlRoot: raiseCapitalServer + '/campaign/:id/general_information',
       template: require('./templates/generalInformation.pug'),
