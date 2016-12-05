ee254ee43d58096cff6b8fada659ff508e78b92d
Arthur Yip <arthuryip723@gmail.com>
2016-12-05 15:34:07 -0500

    fixed see all link not scrolling up

--- src/components/campaign/views.js
+++ src/components/campaign/views.js
@@ -26,6 +26,7 @@ module.exports = {
         })
       );
       this.$el.find('.selectpicker').selectpicker();
+      $('body').scrollTo();
       //selectPicker('.selectpicker');
       return this;
     },
