c0541b297788f0cc4a4c5582be05ec7bae94f397
Arthur Yip <arthuryip723@gmail.com>
2016-12-09 18:41:06 -0500

    move ga place

--- src/components/formc/route.js
+++ src/components/formc/route.js
@@ -27,6 +27,7 @@ module.exports = Backbone.Router.extend({
   },
 
   execute: function (callback, args, name) {
+    ga('send', 'pageview', "/" + Backbone.history.getPath());
     if (app.user.is_anonymous()) {
       const pView = require('components/anonymousAccount/views.js');
       require.ensure([], function() {
@@ -36,7 +37,6 @@ module.exports = Backbone.Router.extend({
       });
       return false;
     }
-    ga('send', 'pageview', "/" + Backbone.history.getPath());
     if (callback) callback.apply(this, args);
     else alert('Not such url');
   },
