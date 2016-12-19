3a77cb6e1bd3bc6fa7ab8048c57ac76ba89a6c4c
Vladyslav <vladka@webdevelop.pro>
2016-12-19 15:51:03 -0500

    updated fields.js for Ostap

--- src/fields.js
+++ src/fields.js
@@ -1,21 +1,125 @@
-function prepareField(name, attr) {
-  if(attr.schema) {
-    attr = _.extend(attr, attr.schema)
-    delete attr.schema;
-  }
-  if(attr.placeholder == null || attr.placeholder == "") {
-    attr.placeholder = attr.label || '';
-  }
-  if(attr.required == true) {
-    attr.label += '<span class="color-red">*</span>';
-  }
-}
+let exports = {
+  prepareNestedField(nestedName, name, value, index, myAttr, schema) {
+    if(value == null) {
+      console.log('value for ' + name + ' is null ')
+      value = {}
+    }
+    _.extend(myAttr, schema);
+    myAttr.value = value[name] ? value[name]: '';
+    myAttr.id = nestedName + '__' + index + '__' + name + '';
+  },
 
+  /* 
+   * VLAD: я не знаю где это используется 
+  nestedTextLabel(nestedName, name, myAttr, vals) {
+    vals = vals || values;
+    myAttr = _.extend(myAttr, fields[nestedName].schema[name]);
+    myAttr.value = vals[name] ? vals[name] : '';
 
-module.exports = {
+    return this.textLabel(
+      nestedName + '[' + name + ']',
+      myAttr
+    )
+  },
+
+  nestedText(nestedName, name, myAttr, vals) {
+    vals = vals || values;
+    myAttr = _.extend(myAttr, fields[nestedName].schema[name]);
+    myAttr.value = vals[name] ? vals[name] : '';
+
+    return this.fieldText(
+      nestedName + '[' + name + ']',
+      myAttr
+    )
+  },
+  */
+
+  nestedTextLabel(nestedName, name, value, index, myAttr, schema) {
+    this.prepareNestedField(nestedName, name, value, index, myAttr, schema);
+    myAttr.class1 = myAttr.class1 ? myAttr.class1 : 'text-lg-right col-lg-3 col-md-12 text-md-left';
+    myAttr.class2 = myAttr.class2 ? myAttr.class2 : 'col-lg-9 col-md-12';
+    return this.textLabel(
+      nestedName + '[' + index + '][' + name + ']',
+      myAttr
+    )
+  },
+
+  nestedTextareaLabel(nestedName, name, value, index, myAttr, schema) {
+    this.prepareNestedField(nestedName, name, value, index, myAttr, schema);
+    myAttr.class1 = myAttr.class1 ? myAttr.class1 : 'text-lg-right col-lg-3 col-md-12 text-md-left';
+    myAttr.class2 = myAttr.class2 ? myAttr.class2 : 'col-lg-9 col-md-12';
+    return this.textareaLabel(
+      nestedName + '[' + index + '][' + name + ']',
+      myAttr
+    )
+  },
+
+  nestedChoiceLabel(nestedName, name, value, index, myAttr, schema) {
+    this.prepareNestedField(nestedName, name, value, index, myAttr, schema);
+    myAttr.class1 = myAttr.class1 ? myAttr.class1 : 'text-lg-right col-lg-3 col-md-12 text-md-left';
+    myAttr.class2 = myAttr.class2 ? myAttr.class2 : 'col-lg-9 col-md-12';
+    return this.choiceLabel(
+      nestedName + '[' + index + '][' + name + ']',
+      myAttr
+    )
+  },
+
+  nestedRadioLabel(nestedName, name, value, index, myAttr, schema) {
+    this.prepareNestedField(nestedName, name, value, index, myAttr, schema);
+    myAttr.class1 = myAttr.class1 ? myAttr.class1 : 'text-lg-right col-lg-3 col-md-12 text-md-left';
+    myAttr.class2 = myAttr.class2 ? myAttr.class2 : 'col-lg-9 col-md-12';
+    return this.radioLabel(
+      nestedName + '[' + index + '][' + name + ']',
+      myAttr
+    )
+  },
+
+  nestedDateDay(nestedName, name, value, index, myAttr, schema) {
+    this.prepareNestedField(nestedName, name, value, index, myAttr, schema);
+    myAttr.class = myAttr.class ? myAttr.class : 'col-lg-6 p-r-0';
+    myAttr.id = myAttr.id + '__day'
+    return this.dateDay(
+      nestedName + '[' + index + '][' + name + ']',
+      myAttr
+    )
+  },
+
+  nestedDateMonth(nestedName, name, value, index, myAttr, schema) {
+    this.prepareNestedField(nestedName, name, value, index, myAttr, schema);
+    myAttr.class = myAttr.class ? myAttr.class : 'col-lg-6 p-r-0';
+    myAttr.id = myAttr.id + '__month'
+    return this.dateMonth(
+      nestedName + '[' + index + '][' + name + ']',
+      myAttr
+    )
+  },
+
+  nestedDateYear(nestedName, name, value, index, myAttr, schema) {
+    this.prepareNestedField(nestedName, name, value, index, myAttr, schema);
+    myAttr.class = myAttr.class ? myAttr.class : 'col-lg-6 p-r-0';
+    myAttr.id = myAttr.id + '__year'
+    return this.dateYear(
+      nestedName + '[' + index + '][' + name + ']',
+      myAttr
+    )
+  },
+
+  prepareField(name, attr) {
+    if(attr.schema) {
+      attr = _.extend(attr, attr.schema)
+      delete attr.schema;
+    }
+    if(attr.placeholder == null || attr.placeholder == "") {
+      attr.placeholder = attr.label || '';
+    }
+    if(attr.required == true) {
+      attr.label += '<span class="color-red">*</span>';
+    }
+  },
+
   textLabel(name, attr) {
     attr.name = name;
-    prepareField(name, attr);
+    this.prepareField(name, attr);
     const template = require('./templates/textLabel.pug');
     return template(attr);
   },
@@ -44,3 +148,5 @@ module.exports = {
     return template(attr);
   }
 }
+
+module.exports = exports; 
