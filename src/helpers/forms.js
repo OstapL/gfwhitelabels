5019032c7e0cedfa314b6b5eac61d7b420c06037
Arthur Yip <arthuryip723@gmail.com>
2016-12-20 19:55:38 -0500

    fixing use of proceeds

--- src/helpers/forms.js
+++ src/helpers/forms.js
@@ -243,6 +243,7 @@ module.exports = {
     _(fields).each((el, key) => {
       if(el.type == 'nested') {
         if(Array.isArray(data[key])) {
+          data[key] = data[key].filter(function(el) { return el !== null;})
           data[key].forEach((el, i) => {
             let emptyValues = 0;
             _(el).each((val, subkey) => {
