c2d2795285f8e577902d489e6629ca288d4e91f9
admin <admin@gf.com>
2016-12-25 19:32:12 +0000

    routes changed, templates added

--- src/components/blog/route.js
+++ src/components/blog/route.js
@@ -2,54 +2,57 @@ const View = require('components/blog/views.js');
 
 module.exports = Backbone.Router.extend({
   routes: {
-    'blog': 'list',
-    'blog/:id': 'detail',
-    'blog/create': 'createEdit',
-    'blog/:id/edit': 'createEdit',
-  },
+      'blog': 'list',
+      'blog/:id': 'detail',
+      'blog/create': 'createEdit',
+      'blog/:id/edit': 'createEdit',
+    },
 
   execute: function (callback, args, name) {
-    if (app.user.is_anonymous() && name == 'createEdit') {
-      const pView = require('components/anonymousAccount/views.js');
-      require.ensure([], function() {
-        new pView.popupLogin().render(window.location.pathname);
-        app.hideLoading();
-        $('#sign_up').modal();
-      });
-      return false;
-    }
-    if (callback) callback.apply(this, args);
+      if (app.user.is_anonymous() && name == 'createEdit') {
+            const pView = require('components/anonymousAccount/views.js');
+            require.ensure([], function() {
+              new pView.popupLogin().render(window.location.pathname);
+              app.hideLoading();
+              $('#sign_up').modal();
+            });
+            return false;
+          }
+     if (callback) callback.apply(this, args);
   },
 
   list() {
-    api.makeCacheRequest(blogServer + '/').
-      then((data) => {
-        const i = View.list({
-          model: data
+      api.makeCacheRequest(blogServer + '/', 'GET').
+        then((data) => {
+          new View.list({
+            model: data
+          }).render();
+          app.hideLoading();
         });
-      });
   },
 
   createEdit (id) {
-    let r = '';
-    if(id) {
-      r = blogServer + '/' + id;
-    }
-    $.when(r).
-      then((data) => {
-        const i = View.list({
+      let r = '';
+      if(id) {
+            r = blogServer + '/' + id;
+          }
+      $.when(r).
+        then((data) => {
+        new View.createEdit({
           model: data
         }).render();
+        app.hideLoading();
       });
   },
 
   detail(id) {
-    api.makeCacheRequest(blogServer + '/' + id).
-      then((data) => {
-        const i = View.list({
-          model: data
+      api.makeCacheRequest(blogServer + '/' + id, 'GET').
+        then((data) => {
+          new View.detail({
+            model: data
+          }).render();
+          app.hideLoading();
         });
-      });
   },
-   
+	   
 });
