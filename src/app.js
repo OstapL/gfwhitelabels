54634dd635a147d71e5fbea57edb9e063168adbf
Vladyslav <vladka@webdevelop.pro>
2016-12-30 13:48:51 -0500

    test

--- src/app.js
+++ src/app.js
@@ -224,10 +224,10 @@ let app = {
 
   runGoogleAnalytics(id) {
     (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
     j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl+ '&gtm_auth=Gp064W3aSXcXjsClBYV7QA&gtm_preview=env-2&gtm_cookies_win=x';f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer',id);
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer', id);
   },
 
 };
