47b44c57ae1b76e0d41cc5db7419fa56729c19a0
OstapL <ostap-v-30@mail.ru>
2017-01-09 13:56:08 +0100

    added min css

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,12 @@ var gulp        = require('gulp'),
     sass        = require('gulp-sass'),
     autoprefixer= require('gulp-autoprefixer'),
     cleanCSS    = require('gulp-clean-css'),
+    cssmin = require('gulp-minify-css'),
     rename      = require('gulp-rename'),
     concat      = require('gulp-concat'),
     uglify      = require('gulp-uglify');
     path        = require('path');
+
 
 
 var config = {
@@ -26,6 +28,7 @@ gulp.task('browser-sync', ['styles', 'scripts'], function() {
     });
 });
 
+
 gulp.task('styles', function () {
     return gulp.src('src/sass/mixins_all.sass')
     .pipe(sass({
@@ -36,12 +39,14 @@ gulp.task('styles', function () {
             config.fontAwesomeDir
         ]
     }).on('error', sass.logError))
-    .pipe(rename({basename: 'main2', prefix : ''}))
+    .pipe(cssmin()) // Сжимаем
+    .pipe(rename({basename: 'main2', suffix : '.min'}))
 // .pipe(autoprefixer({browsers: ['last 15 versions'], cascade: false}))
 // .pipe(cleanCSS())
     .pipe(gulp.dest(config.publicDir + '/css'))
     //.pipe(browserSync.stream());
 });
+
 
 gulp.task('watch', function () {
     gulp.watch(['src/sass/*.sass', 'src/sass/**/*.sass', 'src/sass/**/*.scss' ], ['styles']);
