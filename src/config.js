cc6c2ad40eba67ca6d53ddae96e330c5e1041741
Vladyslav <vladka@webdevelop.pro>
2017-01-24 13:48:36 -0500

    update stripe key

--- src/config.js
+++ src/config.js
@@ -32,5 +32,5 @@ global.linkedinClientId = "77hgtbs9rkjk20";
 
 
 global.googleMapKey = "AIzaSyBpCl9-7bkVISZ0o-AaFCsKzZwGAxalkZU";
-global.stripeKey = "pk_test_Z7YAhlyPtnW7bpd8LJUHTSou";
+global.stripeKey = "pk_live_4ZF2RPjAVJ0tXrDNMauofmgb";
 global.googleAnalyticsId = 'GTM-NC9XW5D';
