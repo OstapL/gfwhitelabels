1a6d7a1cca58d03160c59d2c28774f841544f097
Vladyslav <vladka@webdevelop.pro>
2017-01-25 17:25:19 -0500

    Fixed for DELETE method

--- src/helpers/forms.js
+++ src/helpers/forms.js
@@ -43,7 +43,7 @@ module.exports = {
       url = serverUrl + url
     }
 
-    if(type == 'POST' || type == 'PUT' || type == 'PATCH') {
+    if(type == 'POST' || type == 'PUT' || type == 'PATCH' || type == 'DELETE') {
       data = JSON.stringify(data);
     }
 
