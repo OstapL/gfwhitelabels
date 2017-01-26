9cb79348412f64a7509d123401ebe0b5032f8a27
Vladimir Chagin <vladimir.chagin@gmail.com>
2017-01-26 18:07:08 +0200

    fix investment issues with required US/nonUS fields

--- src/components/campaign/views.js
+++ src/components/campaign/views.js
@@ -513,32 +513,38 @@ module.exports = {
           },
           choices: COUNTRIES
         },
-        // fn: function(name, value, attr, data, schema) {
-        //   return this.required(name, true, attr, data);
-        // },
       });
 
-      this.fields.personal_information_data.schema.phone.fn = function(name, value, attr, data, schema) {
-        let country = this.getData(data, 'personal_information_data.country');
-        if (country == 'US')
-          return;
+      this.fields.personal_information_data.schema.phone = _.extend(this.fields.personal_information_data.schema.phone, {
+        required: false,
+        fn: function(name, value, attr, data, schema) {
+          let country = this.getData(data, 'personal_information_data.country');
+          if (country == 'US')
+            return;
 
-        return this.required(name, true, attr, data);
-      };
+          return this.required(name, true, attr, data);
+        },
+      });
 
-      this.fields.personal_information_data.schema.city.fn = function(name, value, attr, data, schema) {
-        let country = this.getData(data, 'personal_information_data.country');
-        if (country == 'US')
-          return;
-        return this.required(name, true, attr, data);
-      };
+      this.fields.personal_information_data.schema.city = _.extend(this.fields.personal_information_data.schema.city, {
+        fn: function(name, value, attr, data, schema) {
+          let country = this.getData(data, 'personal_information_data.country');
+          if (country == 'US')
+            return;
+          return this.required(name, true, attr, data);
+        },
+        required: false,
+      });
 
-      this.fields.personal_information_data.schema.state.fn = function(name, value, attr, data, schema) {
-        let country = this.getData(data, 'personal_information_data.country');
-        if (country == 'US')
-          return;
-        return this.required(name, true, attr, data);
-      };
+      this.fields.personal_information_data.schema.state = _.extend(this.fields.personal_information_data.schema.state, {
+        required: false,
+        fn: function(name, value, attr, data, schema) {
+          let country = this.getData(data, 'personal_information_data.country');
+          if (country == 'US')
+            return;
+          return this.required(name, true, attr, data);
+        },
+      });
 
       this.user.ssn_re = this.user.ssn;
 
