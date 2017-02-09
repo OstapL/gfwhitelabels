8c57e27baa0326b3a0dc286b696e78e3630344b5
Vladyslav <vladka@webdevelop.pro>
2017-02-09 16:36:30 -0500

    Fixed document links and thank you page

--- src/config.js
+++ src/config.js
@@ -13,7 +13,7 @@ global.esignServer = 'https://api-esign.growthfountain.com';
 // global.esignServer = 'https://api-esign.growthfountain.com';
 global.commentsServer = "https://api-comments.growthfountain.com";
 global.blogServer = "https://api-blog.growthfountain.com";
-global.bucketServer = "http://growthfountain-alpha-storage.s3-website-us-west-1.amazonaws.com";
+global.bucketServer = "https://s3.amazonaws.com/growthfountain-alpha-storage";
 
 global.teamName = 'Team';
 global.teamTitle = "Meet The Team";
