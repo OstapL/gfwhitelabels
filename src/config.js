dcb113f1a98505241d23683925b65f113e078398
Vladyslav <vladka@webdevelop.pro>
2017-03-16 15:35:13 -0400

    up

--- src/config.js
+++ src/config.js
@@ -1,21 +1,21 @@
 global.domainUrl = "growthfountain.com"
-global.serverUrl = "https://django-api-dev.growthfountain.com";
-global.raiseCapitalServer = "https://api-raisecapital-dev.growthfountain.com";
-global.raiseCapitalServer = "http://localhost:8080";
-global.authServer = "https://api-auth-dev.growthfountain.com";
+global.serverUrl = "https://django-api.growthfountain.com";
+global.raiseCapitalServer = "https://api-raisecapital.growthfountain.com";
+//global.raiseCapitalServer = "http://localhost:8003";
+global.authServer = "https://api-auth.growthfountain.com";
 //global.authServer = "http://localhost:8002";
-global.formcServer = "https://api-formc-dev.growthfountain.com";
+global.formcServer = "https://api-formc.growthfountain.com";
 //global.formcServer = "http://localhost:8001";
-global.investmentServer = "https://api-investment-dev.growthfountain.com";
+global.investmentServer = "https://api-investment.growthfountain.com";
 //global.investmentServer = "http://localhost:8003";
-global.filerServer = "https://api-filer-dev.growthfountain.com";
-global.esignServer = 'https://api-esign-dev.growthfountain.com';
+global.filerServer = "https://api-filer.growthfountain.com";
+global.esignServer = 'https://api-esign.growthfountain.com';
 // global.esignServer = 'https://api-esign.growthfountain.com';
-global.commentsServer = "https://api-comments-dev.growthfountain.com";
-global.blogServer = "https://api-blog-dev.growthfountain.com";
+global.commentsServer = "https://api-comments.growthfountain.com";
+global.blogServer = "https://api-blog.growthfountain.com";
+global.notificationsServer = "https://notifications.growthfountain.com/";
 global.bucketServer = "https://s3.amazonaws.com/growthfountain-alpha-storage";
-// global.notificationsServer = "https://notifications.growthfountain.com";
-global.notificationsServer = "https://notifications-dev.growthfountain.com/";
+
 global.teamName = 'Team';
 global.teamTitle = "Meet The Team";
 //global.serverUrl = "http://192.168.99.100:8000";
@@ -27,11 +27,11 @@ global.teamTitle = "Meet The Team";
 // global.googleClientId = "488593151885-87nqfd8gl444a1me0n149otrf37dbahq.apps.googleusercontent.com";
 // global.linkedinClientId = "77wzj6tz0yyr33";
 
-global.facebookClientId = "1405768896335643";
+global.facebookClientId = "1071081646296574";
 global.googleClientId = "372921150-paa9eek64iuo2d5pmg7nraivc72g0sfo.apps.googleusercontent.com";
-global.linkedinClientId = "77wzj6tz0yyr33";
+global.linkedinClientId = "77ibchutnkhnd7";
 
 
 global.googleMapKey = "AIzaSyBpCl9-7bkVISZ0o-AaFCsKzZwGAxalkZU";
-global.stripeKey = "pk_test_Z7YAhlyPtnW7bpd8LJUHTSou";
+global.stripeKey = "pk_live_4ZF2RPjAVJ0tXrDNMauofmgb";
 global.googleAnalyticsId = 'GTM-NC9XW5D';
