b7f9afb5624c1355fd93734055f9ac9932a364a9
Vladimir Chagin <vladimir.chagin@gmail.com>
2017-03-28 18:41:22 +0300

    fx production config

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -15,7 +15,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, './dist'),
     filename: '[name].bundle.[hash].js',
-    pathinfo: true,
+    publicPath: '/',
   },
   module: {
     rules: [
