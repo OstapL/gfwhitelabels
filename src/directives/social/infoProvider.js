84dd7a035ba30c07594073dfb12700c4dfe81c71
Vladimir Chagin <vladimir.chagin@gmail.com>
2017-03-30 20:29:53 +0300

    change: refactor social share directive - add ShareInfo provider for social directives
    closes 439

---
+++ src/directives/social/infoProvider.js
@@ -0,0 +1,31 @@
+class InfoProvider {
+  constructor(model) {
+    this.model = model;
+  }
+
+  twitter() {
+    throw 'Not implemented';
+  }
+
+  linkedin() {
+    throw 'Not implemented';
+  }
+
+  facebook() {
+    throw 'Not implemented';
+  }
+
+  email() {
+    throw 'Not implemented';
+  }
+
+  google_plus() {
+    throw 'Not implemented';
+  }
+
+  confirmationMessage() {
+    return '';
+  }
+}
+
+module.exports = InfoProvider;
