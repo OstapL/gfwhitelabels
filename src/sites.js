88986c35ca56492025b45abe339288df246f195e
Vladyslav <vladka@webdevelop.pro>
2017-04-11 09:58:29 -0400

    Added sites bucket links

---
+++ src/sites.js
@@ -0,0 +1,9 @@
+let exportData = {
+  4: 'http://growthfountain-localhost.s3-website-us-east-1.amazonaws.com',
+  23: 'http://growthfountain-master-storage.s3-website-us-east-1.amazonaws.com',
+  getId() {
+    return 4;
+  }
+};
+
+module.exports = exportData; 
