7ac6991895d78246f87de1f94cc0605a43ce0099
Vladimir Chagin <vladimir.chagin@gmail.com>
2017-04-13 17:10:20 +0300

    fix: show signup popup if #content is not empty

--- src/components/accountProfile/user.js
+++ src/components/accountProfile/user.js
@@ -160,15 +160,20 @@ class User {
 
   ensureLoggedIn(next) {
     if (this.is_anonymous()) {
+      let $content = $('#content');
+
       const pView = require('components/anonymousAccount/views.js');
-      let v = new pView.login({
-        el: '#content',
-        next: next || window.location.pathname,
-        model: {},
-      });
-      // let v = new pView.popupLogin({
-      //   next: next || window.location.pathname,
-      // });
+
+      let v = $content.is(':empty')
+        ? new pView.login({
+            el: '#content',
+            next: next || window.location.pathname,
+            model: {},
+          })
+        : new pView.popupLogin({
+          next: next || window.location.pathname,
+        });
+
       v.render();
       app.hideLoading();
 
