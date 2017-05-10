e3262dbccb4cab1081fd063045efc5a7b73cda50
Vladimir Chagin <vladimir.chagin@gmail.com>
2017-05-10 14:17:17 +0300

    fix #442: absent decimal total_amount part

--- src/components/campaign/views.js
+++ src/components/campaign/views.js
@@ -855,11 +855,11 @@ module.exports = {
         investedPastYear, investedOnOtherSites);
     },
 
-    getInt(value) {
-      return parseInt(value.replace(/\,/g, ''));
+    getNumber(value) {
+      return Number(value.replace(/\,/g, ''));
     },
 
-    formatInt(value) {
+    formatNumber(value) {
       return value.toLocaleString('en-US');
     },
 
@@ -870,7 +870,7 @@ module.exports = {
       if (this.model.campaign.security_type == 1)
         return;
 
-      let amount = this.getInt(e.target.value);
+      let amount = this.getNumber(e.target.value);
       if (!amount)
         return;
 
@@ -883,7 +883,7 @@ module.exports = {
 
       let newAmount = Math.ceil(amount / pricePerShare) *  pricePerShare;
 
-      this.$amount.val(this.formatInt(newAmount));
+      this.$amount.val(this.formatNumber(newAmount));
       this._updateTotalAmount();
 
       if (newAmount > amount) {
@@ -899,11 +899,11 @@ module.exports = {
         return;
       }
 
-      let amount = this.getInt(e.currentTarget.value);
+      let amount = this.getNumber(e.currentTarget.value);
       if (!amount)
         return;
 
-      e.currentTarget.value = this.formatInt(amount);
+      e.currentTarget.value = this.formatNumber(amount);
 
       this.$amount.data('rounded', false);
 
@@ -932,8 +932,8 @@ module.exports = {
     _updateTotalAmount() {
       const feeInfo = this.calcFeeWithCredit();
 
-      let totalAmount = this.getInt(this.$amount.val()) + feeInfo.fee;
-      let formattedTotalAmount = '$' + this.formatInt(totalAmount)
+      let totalAmount = this.getNumber(this.$amount.val()) + feeInfo.fee;
+      let formattedTotalAmount = '$' + this.formatNumber(totalAmount)
       this.$el.find('.total-investment-amount').text(formattedTotalAmount);
       this.$el.find('[name=total_amount]').val(formattedTotalAmount);
 
