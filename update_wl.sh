f20cdf8efef88e576f8334bb0c8eb361788ecb10
Vladyslav <vladka@webdevelop.pro>
2017-05-11 10:52:52 -0400

    update update wl script

--- update_wl.sh
+++ update_wl.sh
@@ -4,15 +4,25 @@ git checkout alpha > /dev/null
 for b in alph-dcu alpha-momentum3 alpha-river-mark alpha-jdcu
 do
     git checkout $b
+    git pull origin $b
+
+    echo "========================= branch $b =========================="
+    if git branch --all | grep --quit "remove/vladyslav2/$b"; then
+        echo "============ MERGE WITH VLADYSLAV/$b ============"
+        git merge vladyslav2/$b > /dev/null
+    fi
+
     git merge --no-ff alpha > /dev/null
     ./fix_merge.sh
+
     if git st | grep --quiet 'UU '; then
-        echo "Error in $b"
+        echo "Error after merge"
         git st | grep 'UU '
         break;
     else
-        echo "Succesfull merged $b"
+        echo "Succesfull merged ALPHA WITH $b"
     fi
+    echo ""
 
 done
 
