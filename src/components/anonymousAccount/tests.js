2b1be2481bb6970ee2ea875e58a89c75486187c4
Vladimir Chagin <vladimir.chagin@gmail.com>
2017-05-22 22:41:13 +0300

    login/signup tests

--- src/components/anonymousAccount/tests.js
+++ src/components/anonymousAccount/tests.js
@@ -5,11 +5,69 @@ const expect    = chai.expect;
 const Views = require('src/components/anonymousAccount/views.js');
 
 describe('Login/Signup tests', () => {
-  it('Login', () => {
-    const view = new Views.login({
+  it('Login form', () => {
+    new Views.login({
       el: '#content',
       model: {}
-    });
-    view.render();
+    }).render();
+    const $loginForm = $('.login-form');
+    $loginForm.find('input[name=email]').val('test@test.com');
+    $loginForm.find('input[name=password]').val('qweqwe123');
+
+    const data = $loginForm.serializeJSON();
+    expect(data.domain).to.equal('alpha.growthfountain.com');
+    expect(data.email).to.equal('test@test.com');
+    expect(data.password).to.equal('qweqwe123');
+  });
+  it('Signup form', () => {
+    new Views.signup({
+      el: '#content',
+      model: {}
+    }).render();
+
+    const $signupForm = $('.signup-form');
+    $signupForm.find('input[name=email]').val('test@test.com');
+    $signupForm.find('input[name=password1]').val('qweqwe123');
+    $signupForm.find('input[name=password2]').val('qweqwe123');
+    $signupForm.find('input[name="checkbox1"]').prop('checked', true);
+
+    const data = $signupForm.serializeJSON();
+    expect(data.domain).to.equal('alpha.growthfountain.com');
+    expect(data.email).to.equal('test@test.com');
+    expect(data.password1).to.equal('qweqwe123');
+    expect(data.password2).to.equal(data.password1);
+    expect(data.checkbox1).to.equal(1);
+  });
+  it('Popup Login form', () => {
+    new Views.popupLogin({
+      el: '#content',
+      model: {}
+    }).render();
+    const $loginForm = $('#sign-in-form');
+    $loginForm.find('input[name=email]').val('test@test.com');
+    $loginForm.find('input[name=password]').val('qweqwe123');
+
+    const data = $loginForm.serializeJSON();
+    expect(data.domain).to.equal('alpha.growthfountain.com');
+    expect(data.email).to.equal('test@test.com');
+    expect(data.password).to.equal('qweqwe123');
+  });
+  it('Popup Signup form', () => {
+    new Views.popupLogin({
+      el: '#content',
+      model: {}
+    }).render();
+    const $signupForm = $('#sign-up-form');
+    $signupForm.find('input[name=email]').val('test@test.com');
+    $signupForm.find('input[name=password1]').val('qweqwe123');
+    $signupForm.find('input[name=password2]').val('qweqwe123');
+    $signupForm.find('input[name="checkbox1"]').prop('checked', true);
+
+    const data = $signupForm.serializeJSON();
+    expect(data.domain).to.equal('alpha.growthfountain.com');
+    expect(data.email).to.equal('test@test.com');
+    expect(data.password1).to.equal('qweqwe123');
+    expect(data.password2).to.equal(data.password1);
+    expect(data.checkbox1).to.equal(1);
   });
 });
--- test/init.js
+++ test/init.js
@@ -27,12 +27,51 @@ global.document = window.document;
 
 global.window.localStorage = global.localStorage;
 global.navigator = {userAgent: 'node.js'};
-global.$ = require('jquery')(window);
+global.$ = global.jQuery = require('jquery');
 global._ = require('underscore');
 global.Backbone = require('backbone');
+global.Tether = window.Tether = require('tether');
+require('bootstrap');
+require('babel-polyfill');
+// require('js/html5-dataset.js');
+// require('classlist-polyfill');
 // global.require = require;
 global.api = require('src/helpers/forms.js');
 const App = require('../src/app.js');
 global.app = App();
 app.user = {};
-
+$.fn.scrollTo = function (padding=0) {
+  $('html, body').animate({
+    scrollTop: $(this).offset().top - padding + 'px',
+  }, 'fast');
+  return this;
+};
+require('jquery-serializejson');
+$.serializeJSON.defaultOptions = _.extend($.serializeJSON.defaultOptions, {
+  customTypes: {
+    decimal(val) {
+      return app.helpers.format.unformatPrice(val);
+    },
+    money(val) {
+      return app.helpers.format.unformatPrice(val);
+    },
+    integer(val) {
+      return parseInt(val);
+    },
+    url(val) {
+      return String(val);
+    },
+    text(val) {
+      return String(val);
+    },
+    email(val) {
+      return String(val);
+    },
+    password(val) {
+      return String(val);
+    },
+  },
+  useIntKeysAsArrayIndex: true,
+  parseNulls: true,
+  parseNumbers: true
+});
