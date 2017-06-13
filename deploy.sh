afdfa47cfaf9abcb636ce8e825eb403e7438aecc
Vladyslav <vladka@webdevelop.pro>
2017-06-13 15:15:05 +0300

    copy index to error

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,7 @@ BUCKET=`echo $BUCKET | sed -e "s/_/-/g"`
 BUCKETSTORAGE="growthfountain-$CIRCLE_BRANCH-storage"
 BUCKETSTORAGE=`echo $BUCKETSTORAGE | sed -e "s/_/-/g"`
 DIR=dist
+cp $DIR/index.html $DIR/error.html
 aws  s3 sync $DIR s3://$BUCKET/ --profile "$PROFILE" > /dev/null
 DIR=src/img/
 aws  s3 sync $DIR s3://$BUCKET/img/ --profile "$PROFILE" > /dev/null
