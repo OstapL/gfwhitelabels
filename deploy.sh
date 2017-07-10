d6a1da89dee39c5b3c60eadc08b34658d1347b3c
Vladyslav <vladka@webdevelop.pro>
2017-07-10 14:03:25 +0300

    Updated consts staticdata for rivermarkcu

--- deploy.sh
+++ deploy.sh
@@ -5,9 +5,10 @@ BUCKET="growthfountain-$CIRCLE_BRANCH"
 BUCKET=`echo $BUCKET | sed -e "s/_/-/g"`
 BUCKETSTORAGE="growthfountain-$CIRCLE_BRANCH-storage"
 BUCKETSTORAGE=`echo $BUCKETSTORAGE | sed -e "s/_/-/g"`
-aws s3 rm s3://$BUCKET --recursive
 cp dist/index.html dist/error.html
 DIR=dist
 aws  s3 sync $DIR s3://$BUCKET/ --profile "$PROFILE" > /dev/null
+aws s3api put-object --bucket $BUCKET --key index.html --cache-control no-cache --body $DIR/index.html --content-type text/html
+aws s3api put-object --bucket $BUCKET --key error.html --cache-control no-cache --body $DIR/error.html --content-type text/html
 DIR=src/js/
 aws  s3 sync $DIR s3://$BUCKET/js/ --profile "$PROFILE" > /dev/null
