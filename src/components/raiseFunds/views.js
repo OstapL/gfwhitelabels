2b6c5acfdfe6d1ab369fee665236566c12b0a684
Vladimir Chagin <vladimir.chagin@gmail.com>
2017-07-24 14:04:17 +0300

    fix #769: fix typo in analytics event

--- src/components/raiseFunds/views.js
+++ src/components/raiseFunds/views.js
@@ -219,7 +219,7 @@ module.exports = {
         localStorage.setItem('user', JSON.stringify(app.user.toJSON()));
       }
 
-      app.analytics.emitEvent(app.analytics.events.CampaignStarted, app.users.stats);
+      app.analytics.emitEvent(app.analytics.events.CampaignStarted, app.user.stats);
 
       app.routers.navigate(
         '/campaign/' + data.campaign_id + '/general_information',
