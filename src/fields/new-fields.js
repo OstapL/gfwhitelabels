1580a3ae9abe0479fdb089447b5114b007b8130c
Vladimir Chagin <vladimir.chagin@gmail.com>
2017-08-08 15:40:18 +0300

    field render changes

--- src/fields/new-fields.js
+++ src/fields/new-fields.js
@@ -9,16 +9,54 @@ const FieldStaticProps = {
 const Field = Backbone.View.extend({
   tagName: 'div',
   events: {
-    'change input': 'onChange'
+    'change input': 'onChange',
   },
 
   initialize(options) {
-    this.schema = options.schema;
-    this.model = options.model;
+    _.extend(this, _.pick(options, [
+      'schema',
+      'model',
+      'name',
+      'getValue',
+      'setValue',
+    ]));
+  },
+
+  buildAttributes() {
+    return {
+
+    };
   },
 
   render() {
+    this.$el.html(
+      this.template({
+        value: this.getValue(),
+        schema: this.schema,
+        attr: {
+          id: 'input-ID',
+          placeholder: 'Placeholder text',
+          helpText: 'Help text',
+          readonly: false,
+          disabled: false,
+          fieldContainerClass: 'form-group row required',
+          labelContainerClass: '',
+          inputContainerClass: '',
+        }
+      })
+    );
+  },
 
+  getValue() {
+    this.model.get(this.name);
+  },
+
+  setValue(value) {
+    this.model.set(this.name, value);
+  },
+
+  getDisplayValue() {
+    return this.getValue();
   },
 
   destroy() {
@@ -26,14 +64,10 @@ const Field = Backbone.View.extend({
     this.$el.remove();
   },
 
-  onChange(e) {
-
-  }
-
 }, FieldStaticProps);
 
 const TextField = Field.extend({
-  template: require('./templates/'),
+  template: require('./templates/text.pug'),
 });
 
 const TextareaField = Field.extend({
@@ -54,6 +88,18 @@ const EmailField = Field.extend({
   render() {
 
   },
+
+});
+
+const URLField = TextField.extend({
+
+});
+
+const SocialNetworkField = URLField.extend({
+
+});
+
+const PasswordField = TextField.extend({
 
 });
 
@@ -97,8 +143,13 @@ module.exports = {
   EmailField,
   TextField,
   TextareaField,
+  URLField,
+  SocialNetworkField,
+  PasswordField,
   SelectField,
   DateField,
+  RadioGroupField,
+  CheckboxField,
   MoneyField,
   PercentField,
   NestedField,
