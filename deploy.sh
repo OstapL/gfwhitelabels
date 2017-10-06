292165a21b7e29f8230e042e8ae6de073ca6ee71
OstapL <ostap-v-30@mail.ru>
2017-10-06 21:16:43 +0300

    widget #923

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,8 @@ cp staticdata/img/logo.png dist/staticdata/img/logo.png
 cp staticdata/img/fade-in-logo.png dist/staticdata/img/fade-in-logo.png
 mkdir dist/staticdata/img/email
 cp staticdata/img/email/header-email.png dist/staticdata/img/email/header-email.png
+cp src/js/widget/widget.js dist/js/widget/widget.js
+cp src/js/widget/widget.css dist/js/widget/widget.css
 DIR=dist
 aws  s3 sync $DIR s3://$BUCKET/ --profile "$PROFILE" > /dev/null
 aws s3api put-object --bucket $BUCKET --key index.html --cache-control no-cache --body $DIR/index.html --content-type text/html
