0d38f249709695cdeb01d66bc335d08dd315579f
Vladyslav <vladka@webdevelop.pro>
2017-10-09 20:54:06 +0300

    Small fix for loader

--- src/components/campaign/route.js
+++ src/components/campaign/route.js
@@ -184,9 +184,9 @@ module.exports = {
             iframe.setAttribute('style', "position:fixed; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;");
             iframe.setAttribute('src', "data:application/pdf;base64," + app.utils.base64Encode(rawData));
 
-            $('body').scrollTo();
-            app.hideLoading();
           }
+          // $('body').scrollTo();
+          app.hideLoading(300);
 
         });
       }, 'campaign_chunk');
