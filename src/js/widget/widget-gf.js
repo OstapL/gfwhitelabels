f1c3c14734d9008274000ee44fe26cbeb776bb9e
OstapL <ostap-v-30@mail.ru>
2017-10-09 22:37:35 +0300

    change widget and add test widget page

--- src/js/widget/widget-gf.js
+++ src/js/widget/widget-gf.js
@@ -1,20 +1,29 @@
 function widgetsElement() {
+  //document.body.onload = widgetsElement;
   var headID = document.getElementsByTagName('head')[0];
     // link to css
     var linkCss = document.createElement('link');
     linkCss.type = 'text/css';
     linkCss.rel = 'stylesheet';
-    linkCss.href = 'https://alpha.growthfountain.com/js/widget/widget-gf.css';
+    linkCss.href = 'widget.css';
     headID.appendChild(linkCss);
 
-  var widget = document.querySelector("#widget-gf");
+  var widget = document.querySelector("#gf__widget_invest");
+  var widgetShow = document.querySelector('#gf__widget_show');
+
   setTimeout(function() {
     widget.style.display = 'block';
-  }, 4000);
+    widgetShow.style.display = 'block';
+  }, 3000);
 
   function hide(event) {
     event.stopPropagation();
     widget.style.display = 'none';
   };
-  document.querySelector('#close-widget').addEventListener('click', hide);
+  function show(event) {
+    event.stopPropagation();
+    widget.style.display = 'block';
+  };
+  document.querySelector('#gFcloseWidget').addEventListener('click', hide);
+  document.querySelector('#gf__widget_show').addEventListener('click', show);
 }
