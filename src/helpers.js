f91bc01ed5533ea6b36913b2faa7585a9faf425b
Vladyslav <vladka@webdevelop.pro>
2017-10-10 21:14:30 +0300

    removed position fixed

--- src/helpers.js
+++ src/helpers.js
@@ -42,7 +42,7 @@ module.exports = {
         iframe.setAttribute("height", "100%");
         iframe.setAttribute("width", "100%");
         iframe.setAttribute("type", "application/pdf");
-        iframe.setAttribute('style', "position:fixed; display:block; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999; overflow-y: auto; -webkit-overflow-scrolling:touch;");
+        iframe.setAttribute('style', "display:block; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999; overflow-y: auto; -webkit-overflow-scrolling:touch;");
         iframe.setAttribute('src', "data:application/pdf;base64," + app.utils.base64Encode(rawData));
       }
     }
