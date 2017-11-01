5413157b3ccebd53cf1aa5d36792c1c5d39c7744
Vladyslav <vladka@webdevelop.pro>
2017-11-01 18:04:41 +0200

    Fixed goolge id

--- src/config.js
+++ src/config.js
@@ -1,40 +1,26 @@
 var e = {
   domainUrl: "growthfountain.com",
-  serverUrl: "https://django-api-dev.growthfountain.com",
   raiseCapitalServer: "https://api-raisecapital-dev.growthfountain.com",
-  //raiseCapitalServer: "http://localhost:8003",
   authServer: "https://api-auth-dev.growthfountain.com",
-  //authServer: "http://localhost:8002",
   formcServer: "https://api-formc-dev.growthfountain.com",
-  //formcServer: "http://localhost:8001",
   investmentServer: "https://api-investment-dev.growthfountain.com",
-  //investmentServer: "http://localhost:8003",
   filerServer: "https://api-filer-dev.growthfountain.com",
   esignServer: 'https://api-esign-dev.growthfountain.com',
-  // esignServer: 'https://api-esign.growthfountain.com',
   commentsServer: "https://api-comments-dev.growthfountain.com",
   blogServer: "https://api-blog-dev.growthfountain.com",
   bucketServer: "https://s3.amazonaws.com/growthfountain-alpha-storage",
-  // notificationsServer: "https://notifications.growthfountain.com",
   notificationsServer: "https://notifications-dev.growthfountain.com/",
   teamName: 'GrowthFountain Team',
   teamTitle: "Meet The Team",
   siteTitle: "Crowdfunding | Digital Federal Credit Union",
-  //serverUrl: "http://192.168.99.100:8000",
-  //serverUrl: "http://gfauth.com:8000",
 
-  // Growth Fountain production keys:
-  // facebookClientId: "191471871275050",
-  // googleClientId: "488593151885-87nqfd8gl444a1me0n149otrf37dbahq.apps.googleusercontent.com",
-  // linkedinClientId: "77wzj6tz0yyr33",
-  
   facebookClientId: "1405768896335643",
   googleClientId: "805823281871-0sbsf2btjd5j13g5aa7sfo4pfjorn3k2.apps.googleusercontent.com",
   linkedinClientId: "77wzj6tz0yyr33",
 
   googleMapKey: "AIzaSyBpCl9-7bkVISZ0o-AaFCsKzZwGAxalkZU",
   stripeKey: "pk_test_Z7YAhlyPtnW7bpd8LJUHTSou",
-  googleTagId: 'GTM-WP7K455',
+
   indexPage: {
     //main slider or img (slider - 1/ img - 0)
     slider: 1,
