a99dd8024caf6ed7277a1a4c1b3fa00db1321b47
Vladyslav <vladka@webdevelop.pro>
2017-11-01 17:52:00 +0200

    Fixed google id

--- src/config.js
+++ src/config.js
@@ -1,6 +1,5 @@
 var e = {
   domainUrl: "growthfountain.com",
-  serverUrl: "https://django-api-dev.growthfountain.com",
   raiseCapitalServer: "https://api-raisecapital-dev.growthfountain.com",
   authServer: "https://api-auth-dev.growthfountain.com",
   formcServer: "https://api-formc-dev.growthfountain.com",
@@ -14,27 +13,14 @@ var e = {
   teamName: 'GrowthFountain Team',
   teamTitle: "Meet The Team",
   siteTitle: 'Crowdfunding | Infinity Federal Credit Union',
-  //serverUrl: "http://192.168.99.100:8000",
-  //serverUrl: "http://gfauth.com:8000",
 
-  // Growth Fountain production keys:
-  // facebookClientId: "191471871275050",
-  // googleClientId: "488593151885-87nqfd8gl444a1me0n149otrf37dbahq.apps.googleusercontent.com",
-  // linkedinClientId: "77wzj6tz0yyr33",
-
-  facebookClientId: "1405768896335643",
-  googleClientId: "805823281871-0sbsf2btjd5j13g5aa7sfo4pfjorn3k2.apps.googleusercontent.com",
-  linkedinClientId: "77wzj6tz0yyr33",
+  facebookClientId = "547857385398592",
+  googleClientId = "372921150-paa9eek64iuo2d5pmg7nraivc72g0sfo.apps.googleusercontent.com",
+  linkedinClientId = "77hgtbs9rkjk20",
 
   googleMapKey: "AIzaSyBpCl9-7bkVISZ0o-AaFCsKzZwGAxalkZU",
   stripeKey: "pk_test_Z7YAhlyPtnW7bpd8LJUHTSou",
 
-  //analytics services settings
-  googleTagID: 'GTM-WP7K455',
-  googleAnalyticsID: 'UA-97185485-1',
-  googleAnalyticsIDGeneral: 'UA-97185485-3',
-  facebookPixelID: 1020185798119090,
-  yandexMetricaID: 44164214,
   indexPage: {
     //main slider or img (slider - 1/ img - 0)
     slider: 0,
